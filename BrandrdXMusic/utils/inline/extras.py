a01bf5af98d55aa822b3d9ab0572f1337808ff0a
Diven <Divenmail07@gmail.com>
2025-11-04 20:56:39 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🎗️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🎗️", url=f"https://t.me/Infvibe"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
